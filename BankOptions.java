6276cfccc383a641c73e11f8bb3a728d71cd1f2c
Matt Ostlind <mostlind@gmail.com>
2016-03-22 14:42:14 -0500

    adds comments

--- BankOptions.java
+++ BankOptions.java
@@ -1,14 +1,23 @@
+/*
 
+An object of this class is created when login credentials are created.
+It is how the user interacts with the account.
+Creates a new object of Account.
+Will use Account's get and set functions for interacting with account file.
+
+*/
 package BankAccount;
 
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.*; //UI stuff
+import java.awt.event.*; //event-handling stuff
 
 
 
-public class BankOptions extends Frame implements WindowListener{
-	
+public class BankOptions extends Frame implements WindowListener{ //Frame for UI, Window listener                                               for closing whendow. 
+    
+    
+	//new button instances
 	Button depositButton = new Button("Deposit");
 	Button withdrawButton = new Button("Withdraw");
 	Button checkBalanceButton = new Button("Check Balance");
@@ -16,12 +25,12 @@ public class BankOptions extends Frame implements WindowListener{
 	Button changePasswordButton = new Button("Change Password");
 	Button logoutButton = new Button("Log Out");
 	
-	public BankOptions(int accountNumber){
+	public BankOptions(int accountNumber){ //constructor takes account number from login
 		
-		Account account = new Account(accountNumber);
+		Account account = new Account(accountNumber); //passes account number to Account
 		
-
-		setLayout(new GridLayout(3,2));
+        //Sets UI stuff, six buttons in a 3x2 grid, attaches Action listeners
+		setLayout(new GridLayout(3,2)); 
 		
 		add(depositButton);
 		depositButton.addActionListener(new DepositButtonListener());
@@ -38,7 +47,7 @@ public class BankOptions extends Frame implements WindowListener{
 		add(changePasswordButton);
 		changePasswordButton.addActionListener(new ChangePasswordButtonListener());
 		
-		add(logoutButton);
+		add(logoutButton); //logout creates new Login object, 
 		logoutButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent evt){
@@ -51,10 +60,12 @@ public class BankOptions extends Frame implements WindowListener{
 		
 		setVisible(true);
 		
-		addWindowListener(this);
+		addWindowListener(this); //for closing window on pressing x
 		
 	}
-	
+    
+    
+	//Button listeners, code will run when corresponding button is pressed
 	private class DepositButtonListener implements ActionListener{
 		
 		@Override
@@ -92,10 +103,11 @@ public class BankOptions extends Frame implements WindowListener{
 	
 	
 	@Override
-	public void windowClosing(WindowEvent e){
+	public void windowClosing(WindowEvent e){ //closes window when x in top left is clicked
 		System.exit(0);
 	}
 	
+    //not used
 	@Override
 	public void windowClosed(WindowEvent e) {}
 	@Override 
@@ -109,4 +121,4 @@ public class BankOptions extends Frame implements WindowListener{
 	@Override 
 	public void windowDeactivated(WindowEvent e) {}
 
-}
+} //end of BankOptions
--- Login.java
+++ Login.java
@@ -1,39 +1,55 @@
+/*
 
+Login creates a window with a place to enter account number and password.
+Should check if account exists and if account number and password match.
+If they do, creates a new BankOptions object and passes the account number to it.
+Has main(), which creates instance of login.
+
+*/
 package BankAccount;
 
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.io.*;
-import java.util.Scanner;
+import java.awt.*; //UIstuff
+import java.awt.event.*; //event-handling
+import javax.swing.*; //UIstuff with a J in front of it
+import java.io.*; //for File()
+import java.util.Scanner; //Scanner
 
 public class Login extends JFrame implements ActionListener, WindowListener{
+        //extends JFrame makes this a window, ActionListener for event-handling
+        //window listener for window close on 'x'
 	
-	Label acctNumLabel = new Label("Account Number: ");
-	TextField acctNumField = new TextField(30);
-	Label passwdLabel = new Label("Password: ");
-	TextField passwdField = new TextField(30);
-	Button loginButton = new Button("Login");
+    //new UI objects
+	Label acctNumLabel = new Label("Account Number: "); //label in front of acctNumField
+	TextField acctNumField = new TextField(30); //place to enter account number
+	Label passwdLabel = new Label("Password: "); //label designating password field
+	TextField passwdField = new TextField(30); //place to put password
+	Button loginButton = new Button("Login"); //login button
 	
 	
-	public Login()
+	public Login() //constructor
 	{
 		
-		setLayout( new FlowLayout(FlowLayout.RIGHT));
+		setLayout( new FlowLayout(FlowLayout.RIGHT)); //uses flow layout 
 		
-		JPanel acctNumPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		acctNumPanel.add(acctNumLabel);
-		acctNumPanel.add(acctNumField);
+        
+        //creates subcontainer, adds acctNum elements to it, adds action listener
+        //adds all of that to JFrame
+		JPanel acctNumPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT)); 
+                                    //Creates subcontainer
+		acctNumPanel.add(acctNumLabel); //add to JPanel
+		acctNumPanel.add(acctNumField); //add to JPanel
 		acctNumField.addActionListener(this);
-		add(acctNumPanel);
+		add(acctNumPanel); //Adds JPanel with elements to JFrame
 		
+        //Same for passwd Elements
 		JPanel passwdPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		passwdPanel.add(passwdLabel);
 		passwdPanel.add(passwdField);
 		passwdField.addActionListener(this);
 		add(passwdPanel);
 		
+        //Adds button to JFrame, adds action listener
 		add(loginButton);
 		loginButton.addActionListener(this);
 		
@@ -42,48 +58,52 @@ public class Login extends JFrame implements ActionListener, WindowListener{
 		
 		setVisible(true);
 		
-		setResizable(false);
+		setResizable(false); //user can't change window size
 		
-		addWindowListener(this);
+		addWindowListener(this); //for close on x
 		
 		
 	}
 	
 	public void actionPerformed(ActionEvent evt)
 	{
-		Scanner loginStream = null;
-		//boolean accountFound = false;
-		boolean authenticated = false;
+		Scanner loginStream = null; //initializes loginStream, if not initialized, and error is
+                //thrown because the try block might fail. In that case loginStream would never
+                //initialized
 		
+		File accountFile = new File("/Users/mostlind/Documents/loginInfo.txt"); 
+                //Change this for your filesystem
+                //Might use different file i/o object like FileInputStream, but this works
+                //path to .txt file with accountNums and passwords
+                //in format:
+                //accountNum password
+                //accountNum password
 
-		File accountFile = new File("/Users/mostlind/Documents/loginInfo.txt");
-
+        //creates scanner for accountFile
 		try{
 			loginStream = new Scanner(accountFile);
 		}
-		
 		catch(FileNotFoundException e){
-			System.out.println("Account doesn't exist.");
+			System.out.println("File doesn't exist.");
 		}
 		
+        
+        //iterates through accountFile to see if accountNum is in it, then compares password
+        //creates new BankOptions and passes it the account number
+        //deletes Login() object
 		while(loginStream.hasNext()){
 			int accountNumber = loginStream.nextInt();
 			
 			if (accountNumber == Integer.parseInt(acctNumField.getText()))
 			{
-				String password = loginStream.next();
-				System.out.println(password + " " + passwdField.getText());
-				
+				String password = loginStream.next()
 				
 				if (password.equals(passwdField.getText())) {
-				
-					authenticated = true;
 					
 					loginStream.close();
 			
 					new BankOptions(accountNumber);
 
-			
 					this.dispose();
 						
 					break;
@@ -95,27 +115,23 @@ public class Login extends JFrame implements ActionListener, WindowListener{
 			}
 		}
 		
-		
-		/*if ((acctNumField.getText() == "12345") && (passwdField.getText() == "hello"))
-		{
-			new BankOptions();
-			
-			this.dispose();
-		}*/
-		
 		loginStream.close();
 	} //end of actionPerformed
-	
+    
+    
+	//main
 	public static void main(String [] args)
 	{
 		new Login();
 	}
 	
+    //closes window on x
 	@Override
 	public void windowClosing(WindowEvent e){
 		System.exit(0);
 	}
 	
+    //not used
 	@Override
 	public void windowClosed(WindowEvent e) {}
 	@Override 
@@ -131,4 +147,4 @@ public class Login extends JFrame implements ActionListener, WindowListener{
 	
 	
 
-}
+} //end of Login
